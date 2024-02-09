d8bbd52955c9f276c96b5d668197a99fcb0c1c0c
seanbaraka <seanbaraka@gmail.com>
2024-02-09 14:01:47 +0300

    feat: changes the name of the binary

--- bin/nuxi-cached.js
+++ bin/nuxi-cached.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const binName = "nuxt-cached";
+const binName = "nuxi-cached";
 try {
   execFileSync(path.resolve(`${__dirname}/${binName}`), process.argv.slice(2), {
     stdio: "inherit",
